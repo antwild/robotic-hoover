30dcdd0650aa65f2f32a71d15c19f142a4a1942e
Anthony Wildman <anthonywildman@hotmail.co.uk>
2019-06-26 15:43:50 +0100

    Moving and cleaning functions work

--- app.rb
+++ app.rb
@@ -22,19 +22,32 @@ dirt = specs[(2..-2)].each do |array|
   array.map! { |coordinate| coordinate.to_i }
 end
 directions = specs[-1].map! { |direction| direction.capitalize }
+dirt_left = dirt.length
 clean_counter = 0
 
-
-def move(directions, hoover)
-  directions.each do |direction|
-    case direction
-    when "N" then hoover[1] += 1
-    when "S" then hoover[1] -= 1
-    when "E" then hoover[0] += 1
-    when "W" then hoover[0] -= 1
-    end
+directions.each do |direction|
+  case direction
+  when "N" then hoover[1] += 1
+  when "S" then hoover[1] -= 1
+  when "E" then hoover[0] += 1
+  when "W" then hoover[0] -= 1
   end
+
+    dirt.each do |spot|
+      if hoover == spot
+        clean_counter += 1
+        dirt_left -= 1
+        dirt.delete(hoover)
+      end
+    end
 end
 
-move(directions, hoover)
+
 puts "The hoover has finished and is now at position x:#{hoover[0]} y:#{hoover[1]}"
+puts "and cleaned in #{clean_counter} spots"
+if dirt_left < 1
+  puts "The room is clean! Great!"
+else
+  puts "The room still isn't clean. There are #{dirt_left} piles of dirt left..."
+end
+
